7020d54060000fde5e22d7a0bf34b5bfb30ae68f
Pranav <mpranavprem@gmail.com>
2025-05-20 15:32:47 +0530

    changes

--- src/pages/website/ServiceDetails/components/TabServices.jsx
+++ src/pages/website/ServiceDetails/components/TabServices.jsx
@@ -58,7 +58,7 @@ const TabServices = ({ service }) => {
               <h6 className="text-[1.25rem] font-mulish lg:text-xl font-semibold text-center whitespace-pre-line">
                 {service.getStarted.heading}
               </h6>
-              <p className="desc !font-inter !font-normal">
+              <p className="desc !font-inter !font-normal !leading-[1.7] hyphens-auto text-justify">
                 {service.getStarted.desc}
               </p>
             </div>
